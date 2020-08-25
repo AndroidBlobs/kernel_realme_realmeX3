c0fed701cbcd410c777e32219c491fe401567ece
ShivamKumarJha <jha.shivam3@gmail.com>
2020-08-25 22:57:37 +0530

    Add block/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -49,6 +49,17 @@
 struct dentry *blk_debugfs_root;
 #endif
 
+/*Hank.liu@TECH.BSP Kernel IO Latency  2019-03-21,io information*/
+#if defined(VENDOR_EDIT) && defined(CONFIG_OPPO_HEALTHINFO)
+extern void ohm_iolatency_record(struct request * req,unsigned int nr_bytes, int fg, u64 delta_us);
+extern unsigned long ufs_outstanding;
+static u64 latency_count;
+static u32 io_print_count;
+bool       io_print_flag;
+#define    PRINT_LATENCY     500*1000
+#define    COUNT_TIME      24*60*60*1000
+#endif /*VENDOR_EDIT*/
+
 EXPORT_TRACEPOINT_SYMBOL_GPL(block_bio_remap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(block_rq_remap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(block_bio_complete);
@@ -117,6 +128,10 @@ void blk_rq_init(struct request_queue *q, struct request *rq)
 	memset(rq, 0, sizeof(*rq));
 
 	INIT_LIST_HEAD(&rq->queuelist);
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	INIT_LIST_HEAD(&rq->fg_list);
+#endif /*VENDOR_EDIT*/
 	INIT_LIST_HEAD(&rq->timeout_list);
 	rq->cpu = -1;
 	rq->q = q;
@@ -828,6 +843,11 @@ static void blk_rq_timed_out_timer(unsigned long data)
 	kblockd_schedule_work(&q->timeout_work);
 }
 
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+#define FG_CNT_DEF 20
+#define BOTH_CNT_DEF 10
+#endif /*VENDOR_EDIT*/
 struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 {
 	struct request_queue *q;
@@ -857,6 +877,13 @@ struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 			(VM_MAX_READAHEAD * 1024) / PAGE_SIZE;
 	q->backing_dev_info->capabilities = BDI_CAP_CGROUP_WRITEBACK;
 	q->backing_dev_info->name = "block";
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	q->fg_count_max = FG_CNT_DEF;
+	q->both_count_max = BOTH_CNT_DEF;
+	q->fg_count = FG_CNT_DEF;
+	q->both_count = BOTH_CNT_DEF;
+#endif /*VENDOR_EDIT*/
 	q->node = node_id;
 
 	setup_timer(&q->backing_dev_info->laptop_mode_wb_timer,
@@ -864,6 +891,10 @@ struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 	setup_timer(&q->timeout, blk_rq_timed_out_timer, (unsigned long) q);
 	INIT_WORK(&q->timeout_work, NULL);
 	INIT_LIST_HEAD(&q->queue_head);
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	INIT_LIST_HEAD(&q->fg_head);
+#endif /*VENDOR_EDIT*/
 	INIT_LIST_HEAD(&q->timeout_list);
 	INIT_LIST_HEAD(&q->icq_list);
 #ifdef CONFIG_BLK_CGROUP
@@ -1798,6 +1829,11 @@ void blk_init_request_from_bio(struct request *req, struct bio *bio)
 {
 	struct io_context *ioc = rq_ioc(bio);
 
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	if (bio->bi_opf & REQ_FG)
+		req->cmd_flags |= REQ_FG;
+#endif /*VENDOR_EDIT*/
 	if (bio->bi_opf & REQ_RAHEAD)
 		req->cmd_flags |= REQ_FAILFAST_MASK;
 
@@ -2271,6 +2307,75 @@ out:
 }
 EXPORT_SYMBOL(generic_make_request);
 
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+#define SYSTEM_APP_UID 1000
+static bool is_system_uid(struct task_struct *t)
+{
+	int cur_uid;
+	cur_uid = task_uid(t).val;
+	if (cur_uid ==  SYSTEM_APP_UID)
+		return true;
+
+	return false;
+}
+
+static bool is_zygote_process(struct task_struct *t)
+{
+	const struct cred *tcred = __task_cred(t);
+
+	struct task_struct * first_child = NULL;
+	if(t->children.next && t->children.next != (struct list_head*)&t->children.next)
+		first_child = container_of(t->children.next, struct task_struct, sibling);
+	if(!strcmp(t->comm, "main") && (tcred->uid.val == 0) && (t->parent != 0 && !strcmp(t->parent->comm,"init"))  )
+		return true;
+	else
+		return false;
+	return false;
+}
+
+static bool is_system_process(struct task_struct *t)
+{
+	if (is_system_uid(t)) {
+		if (t->group_leader  && (!strncmp(t->group_leader->comm,"system_server", 13) ||
+			!strncmp(t->group_leader->comm, "surfaceflinger", 14) ||
+			!strncmp(t->group_leader->comm, "servicemanager", 14) ||
+			!strncmp(t->group_leader->comm, "ndroid.systemui", 15)))
+				return true;
+	}
+	return false;
+}
+
+bool is_critial_process(struct task_struct *t)
+{
+	if( is_zygote_process(t) || is_system_process(t))
+		return true;
+
+	return false;
+}
+
+bool is_filter_process(struct task_struct *t)
+{
+	if(!strncmp(t->comm,"logcat", TASK_COMM_LEN) )
+		 return true;
+
+	return false;
+}
+static bool high_prio_for_task(struct task_struct *t)
+{
+	int cur_uid;
+
+	if (!sysctl_fg_io_opt)
+		return false;
+
+	cur_uid = task_uid(t).val;
+	if((is_fg(cur_uid) && !is_system_uid(t) && !is_filter_process(t)) || is_critial_process(t))
+		return true;
+
+	return false;
+}
+#endif /*VENDOR_EDIT*/
+
 /**
  * submit_bio - submit a bio to the block device layer for I/O
  * @bio: The &struct bio which describes the I/O
@@ -2310,7 +2415,11 @@ blk_qc_t submit_bio(struct bio *bio)
 				bio_devname(bio, b), count);
 		}
 	}
-
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	if (high_prio_for_task(current))
+		bio->bi_opf |= REQ_FG;
+#endif
 	return generic_make_request(bio);
 }
 EXPORT_SYMBOL(submit_bio);
@@ -2589,6 +2698,10 @@ struct request *blk_peek_request(struct request_queue *q)
 			 * not be passed by new incoming requests
 			 */
 			rq->rq_flags |= RQF_STARTED;
+/*Hank.liu@PSW.BSP Kernel IO Latency  2019-03-19,request start ktime */
+#if defined(VENDOR_EDIT)
+			rq-> block_io_start = ktime_get();
+#endif
 			trace_block_rq_issue(q, rq);
 		}
 
@@ -2662,6 +2775,11 @@ static void blk_dequeue_request(struct request *rq)
 	BUG_ON(ELV_ON_HASH(rq));
 
 	list_del_init(&rq->queuelist);
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+	if (sysctl_fg_io_opt && (rq->cmd_flags & REQ_FG))
+		list_del_init(&rq->fg_list);
+#endif /*VENDOR_EDIT*/
 
 	/*
 	 * the time frame between a request being removed from the lists
@@ -2752,8 +2870,47 @@ bool blk_update_request(struct request *req, blk_status_t error,
 		unsigned int nr_bytes)
 {
 	int total_bytes;
+#if defined(VENDOR_EDIT) && defined(CONFIG_OPPO_HEALTHINFO)
+/*Hank.liu@TECH.BSP Kernel IO Latency	2019-03-19,request complete ktime*/
+	ktime_t now;
+	u64 delta_us;
+	char rwbs[RWBS_LEN];
+#endif
 
 	trace_block_rq_complete(req, blk_status_to_errno(error), nr_bytes);
+/*Hank.liu@TECH.BSP Kernel IO Latency	2019-03-19,request complete ktime*/
+#if defined(VENDOR_EDIT) && defined(CONFIG_OPPO_HEALTHINFO)
+			if(req->tag >= 0 && req->block_io_start > 0)
+			{
+				io_print_flag = false;
+				now = ktime_get();
+				delta_us = ktime_us_delta(now, req->block_io_start);
+				ohm_iolatency_record(req, nr_bytes, current_is_fg(), ktime_us_delta(now, req->block_io_start));
+				trace_block_time(req->q, req, delta_us, nr_bytes);
+
+				if(delta_us > PRINT_LATENCY) { 
+					if((ktime_to_ms(now)) < COUNT_TIME){
+						latency_count ++;
+					}else{
+						latency_count = 0;
+					}
+					io_print_flag = true;
+					blk_fill_rwbs(rwbs,req->cmd_flags, nr_bytes);
+
+					/*if log is continuous, printk the first log.*/
+					if(!io_print_count)
+					  pr_info("[IO Latency]UID:%u,slot:%d,outstanding=0x%lx,IO_Type:%s,Block IO/Flash Latency:(%llu/%llu)LBA:%llu,length:%d size:%d,count=%lld\n",
+							(from_kuid_munged(current_user_ns(),current_uid())),
+							req->tag,ufs_outstanding,rwbs,delta_us,req->flash_io_latency,
+							(unsigned long long)blk_rq_pos(req),
+							nr_bytes >> 9,blk_rq_bytes(req),latency_count);
+					io_print_count++;
+				}
+
+				if(!io_print_flag && io_print_count)
+					io_print_count = 0;
+			}
+#endif
 
 	if (!req->bio)
 		return false;
